5b647dec0e5c1c9aec0131050cd8802434baaff2
Eran Shabi <eranshabi@gmail.com>
2018-06-04 15:22:58 +0300

    refactor tests

--- src/noAsyncWithoutAwaitRule.spec.ts
+++ src/noAsyncWithoutAwaitRule.spec.ts
@@ -3,57 +3,46 @@ import {helper} from './lintRunner';
 const rule = 'no-async-without-await';
 
 describe('noAsyncWithoutAwait Rule', () => {
-   it(`should fail when the async function doesn't have an await`, () => {
-       const src = `
+    describe('Functions', () => {
+        it(`should fail when the async function doesn't have an await`, () => {
+            const src = `
             async function a(){
                let b = 1;
                console.log(b);
                
             }
         `;
-       const result = helper({src, rule});
-       expect(result.errorCount).toBe(1);
-       expect(result.failures[0].getFailure()).toBe('Async function without await is not allowed');
-   });
+            const result = helper({src, rule});
+            expect(result.errorCount).toBe(1);
+            expect(result.failures[0].getFailure()).toBe('Async function without await is not allowed');
+        });
 
-   it(`should fail when the async arrow function doesn't have an await`, () => {
-       const src = `
-            const a = async () => {
-               let b = 1;
-               console.log(b);
-            }
-        `;
-       const result = helper({src, rule});
-       expect(result.errorCount).toBe(1);
-       expect(result.failures[0].getFailure()).toBe('Async function without await is not allowed');
-   });
-
-   it('should not fail when the async function has an await', () => {
-       const src = `
+        it('should not fail when the async function has an await', () => {
+            const src = `
             async function a(){
                let b = 1;
                await console.log(b);
                
             }
         `;
-       const result = helper({src, rule});
-       expect(result.errorCount).toBe(0);
-   });
+            const result = helper({src, rule});
+            expect(result.errorCount).toBe(0);
+        });
 
-   it('should not fail when the async function has an await inside a function call', () => {
-       const src = `
+        it('should not fail when the async function has an await inside a function call', () => {
+            const src = `
             async function a(){
                let b = 1;
                console.log(await b());
                
             }
         `;
-       const result = helper({src, rule});
-       expect(result.errorCount).toBe(0);
-   });
+            const result = helper({src, rule});
+            expect(result.errorCount).toBe(0);
+        });
 
-   it('should fail when the async function has an await inside a arrow function declaration', () => {
-       const src = `
+        it('should fail when the async function has an await inside a arrow function declaration', () => {
+            const src = `
             async function a(){
                let b = 1;
                let b = async () => {
@@ -62,12 +51,12 @@ describe('noAsyncWithoutAwait Rule', () => {
                
             }
         `;
-       const result = helper({src, rule});
-       expect(result.errorCount).toBe(1);
-   });
+            const result = helper({src, rule});
+            expect(result.errorCount).toBe(1);
+        });
 
-   it('should fail when the async function has an await inside a regular function declaration', () => {
-       const src = `
+        it('should fail when the async function has an await inside a regular function declaration', () => {
+            const src = `
             async function a(){
                let b = 1;
                async function f() {
@@ -76,12 +65,12 @@ describe('noAsyncWithoutAwait Rule', () => {
                
             }
         `;
-       const result = helper({src, rule});
-       expect(result.errorCount).toBe(1);
-   });
+            const result = helper({src, rule});
+            expect(result.errorCount).toBe(1);
+        });
 
-   it('should fail when the function is not at the top level', () => {
-       const src = `
+        it('should fail when the function is not at the top level', () => {
+            const src = `
             function a(){
                let b = 1;
                async function f() {
@@ -90,13 +79,28 @@ describe('noAsyncWithoutAwait Rule', () => {
                
             }
         `;
-       const result = helper({src, rule});
-       expect(result.errorCount).toBe(1);
-   });
+            const result = helper({src, rule});
+            expect(result.errorCount).toBe(1);
+        });
+    });
 
-   describe('Classes', () => {
-      it('should check class methods', () => {
-          const src = `
+    describe('Arrow functions', () => {
+        it(`should fail when the async arrow function doesn't have an await`, () => {
+            const src = `
+            const a = async () => {
+               let b = 1;
+               console.log(b);
+            }
+            `;
+            const result = helper({src, rule});
+            expect(result.errorCount).toBe(1);
+            expect(result.failures[0].getFailure()).toBe('Async function without await is not allowed');
+        });
+    });
+
+    describe('Classes', () => {
+        it('should fail when async class method has no await', () => {
+            const src = `
             class A {
                 async b() {
                     console.log(1);
@@ -104,12 +108,12 @@ describe('noAsyncWithoutAwait Rule', () => {
             
             }
         `;
-          const result = helper({src, rule});
-          expect(result.errorCount).toBe(1);
-      });
+            const result = helper({src, rule});
+            expect(result.errorCount).toBe(1);
+        });
 
-      it('should check class methods', () => {
-          const src = `
+        it('should not fail when async class method has await', () => {
+            const src = `
             class A {
                 async b() {
                     await b();
@@ -117,12 +121,12 @@ describe('noAsyncWithoutAwait Rule', () => {
             
             }
         `;
-          const result = helper({src, rule});
-          expect(result.errorCount).toBe(0);
-      });
+            const result = helper({src, rule});
+            expect(result.errorCount).toBe(0);
+        });
 
-      it('should check class methods', () => {
-          const src = `
+        it('should check classes that are inside functions', () => {
+            const src = `
           async () => {
             await a();
             class A {
@@ -133,8 +137,8 @@ describe('noAsyncWithoutAwait Rule', () => {
             }
           };
         `;
-          const result = helper({src, rule});
-          expect(result.errorCount).toBe(1);
-      });
-   });
+            const result = helper({src, rule});
+            expect(result.errorCount).toBe(1);
+        });
+    });
 });
