50f24a372594f99fbca377368d38eed6e7980096
Eran Shabi <eranshabi@gmail.com>
2018-06-05 13:44:27 +0300

    add noFullPackageImport rule

---
+++ src/noFullPackageImportRule.spec.ts
@@ -0,0 +1,37 @@
+import {helper} from './lintRunner';
+const getRule = (options: string[] = ['a-package']) => ({name: 'no-full-package-import', options: options});
+
+describe('noFullPackgeImport Rule', () => {
+    it(`should fail when importing a full package that was in the arguments`, () => {
+        const src = `
+            import * as _ from 'a-package';
+        `;
+        const result = helper({src, rule: getRule()});
+        expect(result.errorCount).toBe(1);
+        expect(result.failures[0].getFailure()).toBe('importing the full package "a-package" is not allowed');
+    });
+
+    it(`should not fail when importing a specific file from package`, () => {
+        const src = `
+            import * as func from 'a-package/func';
+        `;
+        const result = helper({src, rule: getRule()});
+        expect(result.errorCount).toBe(0);
+    });
+
+    it(`should not fail when importing a full package that is not in the options`, () => {
+        const src = `
+            import * as func from 'b-package';
+        `;
+        const result = helper({src, rule: getRule()});
+        expect(result.errorCount).toBe(0);
+    });
+
+    it(`should not fail when importing a specific file from package`, () => {
+        const src = `
+            import * as func from '@wix/a-package';
+        `;
+        const result = helper({src, rule: getRule(['@wix/a-package'])});
+        expect(result.errorCount).toBe(1);
+    });
+});
---
+++ src/noFullPackageImportRule.ts
@@ -0,0 +1,23 @@
+import * as Lint from 'tslint';
+import * as ts from 'typescript';
+
+export class Rule extends Lint.Rules.AbstractRule {
+    static FAILURE_STRING = (packageName) => `importing the full package "${packageName}" is not allowed`;
+
+    public apply(sourceFile: ts.SourceFile): Lint.RuleFailure[] {
+        return this.applyWithWalker(new Walk(sourceFile, this.getOptions()));
+    }
+}
+
+class Walk extends Lint.RuleWalker {
+    visitImportDeclaration(node: ts.ImportDeclaration) {
+        const isNotAllowedPackage = this.getOptions().some(packageName => {
+            const text = node.moduleSpecifier.text;
+            return text.indexOf(packageName) === 0 && text === packageName;
+        });
+
+        if (isNotAllowedPackage) {
+            this.addFailureAtNode(node, Rule.FAILURE_STRING(node.moduleSpecifier.text));
+        }
+    }
+}
