0d1523a8cf7ccc4f99252f0500a6ead6749035b2
Eran Shabi <eranshabi@gmail.com>
2018-06-05 15:20:39 +0300

    fix publish script

--- src/publish.js
+++ src/publish.js
@@ -1,9 +1,7 @@
 const execSync = require('child_process').execSync;
 
 function getLatestVersion() {
-  return JSON.parse(
-    execSync(`npm show wix-tslint-custom-rules --registry=https://registry.npmjs.org/ --json`),
-  ).version;
+  return JSON.parse(execSync(`npm show wix-tslint-custom-rules --registry=https://registry.npmjs.org/ --json`)).version;
 };
 
 execSync(`npm --no-git-tag-version version ${getLatestVersion()}`);
