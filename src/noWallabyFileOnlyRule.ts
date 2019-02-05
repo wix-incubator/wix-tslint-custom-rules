96da67746bf9a22271b53573da7468d304b73dbb
Sagi Isha <sagii@wix.com>
2019-02-05 16:19:22 +0200

    file.only error should be restricted to its comment

--- src/noWallabyFileOnlyRule.ts
+++ src/noWallabyFileOnlyRule.ts
@@ -1,24 +1,25 @@
 import * as Lint from 'tslint';
 import * as ts from 'typescript';
-import {forEachComment} from 'tsutils';
+import { forEachComment } from 'tsutils';
 
 export class Rule extends Lint.Rules.AbstractRule {
-    public apply(sourceFile: ts.SourceFile): Lint.RuleFailure[] {
-        return this.applyWithFunction(sourceFile, walk);
-    }
+  public apply(sourceFile: ts.SourceFile): Lint.RuleFailure[] {
+    return this.applyWithFunction(sourceFile, walk);
+  }
 }
 
 const FILE_ONLY = 'file.only';
 
 const walk = (ctx: Lint.WalkContext<any>): void => {
-    const {sourceFile} = ctx;
-    forEachComment(ctx.sourceFile, (fullText, comment) => {
-        const commentText = fullText.slice(comment.pos, comment.end);
-        if (commentText.includes(FILE_ONLY)) {
-            ctx.addFailureAtNode(
-                sourceFile,
-                'file.only comments are not allowed'
-            );
-        }
-    });
+  const { sourceFile } = ctx;
+  forEachComment(ctx.sourceFile, (fullText, comment) => {
+    const commentText = fullText.slice(comment.pos, comment.end);
+    if (commentText.includes(FILE_ONLY)) {
+      ctx.addFailureAt(
+        comment.pos,
+        comment.end,
+        'file.only comments are not allowed',
+      );
+    }
+  });
 };
